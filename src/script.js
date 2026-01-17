63ef5f955d798b12fa5f618bdbdb659076f5b82f
XWorkUser <xwork.user1@gmail.com>
2026-01-17 20:54:05 +0400

    deleyed link shop and reworked rules page

--- src/script.js
+++ src/script.js
@@ -72,3 +72,18 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
